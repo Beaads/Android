bcb6bb04181abd551d54cf110c5d72f24d06a2d4
Beaads <biacriciuma@hotmail.com>
2022-01-26 10:12:05 -0300

    Inicio.

---
+++ app/src/main/java/com/beatriz/androidlistafuncionariosbea/ui/activity/ListaFuncionariosActivity.java
@@ -0,0 +1,16 @@
+package com.beatriz.androidlistafuncionariosbea.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.beatriz.androidlistafuncionariosbea.R;
+
+public class ListaFuncionariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_funcionarios);
+    }
+}
